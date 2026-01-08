7bff9e138069d03c999a8635dd752b7623a9c9f5
Abhishek Kumar Tiwari <tiwariabhi62065@gmail.com>
2026-01-08 10:45:00 +0530

    Refactor data processing logic and enhance error handling for improved performance and user experience

---
+++ random_code_17.clj
@@ -0,0 +1,20 @@
+(ns random-code-17
+  (:require [clojure.string :as str]))
+
+;; Random code generator
+(defn generate-random-code [length]
+  (let [characters "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    (apply str (take length (repeatedly #(rand-nth characters))))))
+
+(println "Random Code Generator")
+(println (str/join (repeat 20 "=")))
+(println)
+
+(doseq [i (range 1 6)]
+  (let [code (generate-random-code 12)]
+    (println (str "Code #" i ": " code))))
+
+(let [timestamp (java.time.LocalDateTime/now)]
+  (println)
+  (println (str "Generated at: " (.format timestamp (java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss")))))
+
---
+++ random_code_20.scala
@@ -0,0 +1,26 @@
+import scala.util.Random
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+// Random code generator
+object RandomCode20 {
+  def generateRandomCode(length: Int = 10): String = {
+    val characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    (1 to length).map(_ => characters(Random.nextInt(characters.length))).mkString
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println("Random Code Generator")
+    println("=" * 20)
+    println()
+    
+    for (i <- 1 to 5) {
+      val code = generateRandomCode(12)
+      println(s"Code #$i: $code")
+    }
+    
+    val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    println(s"\nGenerated at: $timestamp")
+  }
+}
+
